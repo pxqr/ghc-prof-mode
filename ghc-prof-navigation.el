506ad8fb3a65b0ac6dbf2f82b7831171e100e82b
fmap <glrenderable@gmail.com>
2012-10-12 04:04:39 +0400

    search more aggressive now

--- ghc-prof-navigation.el
+++ ghc-prof-navigation.el
@@ -4,16 +4,21 @@
   (let* ((rec (ghc-prof-report-extract-record (ghc-prof-current-line-content)))
          (function-name (car rec))
          (module-name  (cadr rec))
-         (buffers (ghc-prof-find-buffer-by-module-name module-name))
-         ;; pick the first buffer with the module name 
-         (buffer-def (car buffers)))
-    (when buffer-def
-      (with-current-buffer buffer-def
-        (let ((position (ghc-prof-function-position function-name)))
-          (when position 
-            (switch-to-buffer-other-window buffer-def)
-            (goto-char position)
-            (beginning-of-line)))))))
+         (buffers (remove-if 'nil 
+                             (mapcar (lambda (buf) 
+                                       (with-current-buffer buf
+                                         (let ((position (ghc-prof-function-position function-name)))
+                                           (when position
+                                             (cons buf (cons position nil))))))
+                                     (ghc-prof-find-buffer-by-module-name module-name))))
+         ;; pick the first buffer with the module name and the scc name
+         (buffer-pos (car buffers)))
+    (if buffer-pos
+        (progn 
+          (switch-to-buffer-other-window (car buffer-pos))
+          (goto-char (cadr buffer-pos))
+          (beginning-of-line))
+        (message "Can't find such function."))))
 
 (defun ghc-prof-current-line-content ()
   "Get content of current line in current buffer, but not affect on position. "
